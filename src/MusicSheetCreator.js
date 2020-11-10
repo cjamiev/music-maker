8ab605524e7d6e8907dcf2f117bd4dcf4cc297ae
Chris Varughese <cjamiev@icloud.com>
2020-11-09 21:54:34 -0600

    Initial addition of bass clef.

--- src/MusicSheetCreator.js
+++ src/MusicSheetCreator.js
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 import MusicSymbolGenerator from './MusicSymbols/MusicSymbolGenerator';
 import './music-sheet-creator.css';
 
+const keyList = ['F6', 'E6', 'D6', 'C6', 'B5', 'A5', 'G5', 'F5', 'E5', 'D5', 'C5', 'B4', 'A4', 'G4', 'F4', 'E4', 'D4', 'C4', 'B3', 'A3', 'G3', 'F3', 'E3'];
+const trebleList = ['F', 'E', 'D', 'C', 'B', 'High A', 'G', 'F', 'E', 'D', 'C', 'B', 'A', 'G', 'F', 'E', 'D', 'Middle C', 'B', 'A', 'G', 'F', 'E'];
+const bassList = ['A', 'G', 'F', 'E', 'D', 'Middle C', 'B', 'A', 'G', 'F', 'E', 'D', 'C', 'B', 'A', 'G', 'F', 'Low E', 'D', 'C', 'B', 'A', 'G'];
 const INDEX_ZERO = 0;
 const AT_LEAST_ONE = 1;
-const DEFAULT_SYMBOL = {
+const DEFAULT_TREBLE = {
   key: 'C4',
   type: 'quarter',
   dotted: false,
@@ -13,73 +16,66 @@ const DEFAULT_SYMBOL = {
   sharp: false,
   fermata: false
 };
+const DEFAULT_BASS = {
+  ...DEFAULT_TREBLE,
+  key: 'A5'
+};
 
-const DisplaySelection = ({ sheet, selectItem, currentIndex }) => {
-  const displayNotes = sheet.map((symbol, index) => {
-    const className = currentIndex === index ? 'display-note display-note-active' : 'display-note';
+const DisplaySelection = ({ trebleStaff, currentTrebleIndex, selectTrebleItem, bassStaff, currentBassIndex, selectBassItem }) => {
+  const trebleNotes = trebleStaff.map((symbol, index) => {
+    const className = currentTrebleIndex === index ? 'display-note display-note-active' : 'display-note';
     return (
-      <div key={symbol.key + index} className={className} onClick={() => { selectItem(index); }}>
+      <div key={symbol.key + index} className={className} onClick={() => { selectTrebleItem(index); }}>
+        <MusicSymbolGenerator symbol={symbol} />
+      </div>
+    );
+  });
+
+  const bassNotes = bassStaff.map((symbol, index) => {
+    const className = currentBassIndex === index ? 'display-note display-note-active' : 'display-note';
+    return (
+      <div key={symbol.key + index} className={className} onClick={() => { selectBassItem(index); }}>
         <MusicSymbolGenerator symbol={symbol} />
       </div>
     );
   });
 
   return (
-    <div className="display-selection">
-      {displayNotes}
+    <div>
+      <div className="display-selection">
+        {trebleNotes}
+      </div >
+      <div className="display-selection">
+        {bassNotes}
+      </div >
     </div>
   );
 };
 
-const SymbolSelector = ({ selectKey, selectNoteType, selectDotted, selectStacatto, selectSymbol }) => {
+const SymbolSelector = ({ selectKey, selectNoteType, selectDotted, selectStacatto, selectSymbol, isTreble }) => {
+  const noteLabels = isTreble ? trebleList : bassList;
+  const renderNotes = keyList.map((item, index) => {
+    return (<button key={item + index} className='note-btn' onClick={() => selectKey(item)}>{noteLabels[index]}</button>);
+  });
+
   return (
     <div className="symbol-selection">
       <div className="column-selection">
         <button className="add-btn" onClick={() => { selectSymbol('wholerest'); }}>Whole Rest</button>
         <button className="add-btn" onClick={() => { selectSymbol('halfrest'); }}>Half Rest</button>
-        <button className="add-btn" onClick={() => { selectSymbol('quarterrest'); }}>Quarter Rest</button>
+        {/* <button className="add-btn" onClick={() => { selectSymbol('quarterrest'); }}>Quarter Rest</button>
         <button className="add-btn" onClick={() => { selectSymbol('8threst'); }}>8th Rest</button>
-        <button className="add-btn" onClick={() => { selectSymbol('16threst'); }}>16th Rest</button>
+        <button className="add-btn" onClick={() => { selectSymbol('16threst'); }}>16th Rest</button> */}
       </div>
       <div className="column-selection">
         <button className="add-btn" onClick={() => { selectNoteType('whole'); }}>Whole Note</button>
         <button className="add-btn" onClick={() => { selectNoteType('half'); }}>Half Note</button>
         <button className="add-btn" onClick={() => { selectNoteType('quarter'); }}>Quarter Note</button>
         <button className="add-btn" onClick={() => { selectDotted(); }}>Dotted</button>
-        <button className="add-btn" onClick={() => { selectStacatto(); }}>Stacatto</button>
+        {/* <button className="add-btn" onClick={() => { selectStacatto(); }}>Stacatto</button> */}
       </div>
-      <div>
-        <div className="note-selection">
-          <button className='note-btn' onClick={() => selectKey('C6')}>C</button>
-          <button className='note-btn' onClick={() => selectKey('D6')}>D</button>
-          <button className='note-btn' onClick={() => selectKey('E6')}>E</button>
-          <button className='note-btn' onClick={() => selectKey('F6')}>F</button>
-        </div>
-        <div className="note-selection">
-          <button className='note-btn' onClick={() => selectKey('C5')}>C</button>
-          <button className='note-btn' onClick={() => selectKey('D5')}>D</button>
-          <button className='note-btn' onClick={() => selectKey('E5')}>E</button>
-          <button className='note-btn' onClick={() => selectKey('F5')}>F</button>
-          <button className='note-btn' onClick={() => selectKey('G5')}>G</button>
-          <button className='note-btn' onClick={() => selectKey('A5')}>High A</button>
-          <button className='note-btn' onClick={() => selectKey('B5')}>B</button>
-        </div>
-        <div className="note-selection">
-          <button className='note-btn' onClick={() => selectKey('C4')}>Middle C</button>
-          <button className='note-btn' onClick={() => selectKey('D4')}>D</button>
-          <button className='note-btn' onClick={() => selectKey('E4')}>E</button>
-          <button className='note-btn' onClick={() => selectKey('F4')}>F</button>
-          <button className='note-btn' onClick={() => selectKey('G4')}>G</button>
-          <button className='note-btn' onClick={() => selectKey('A4')}>A</button>
-          <button className='note-btn' onClick={() => selectKey('B4')}>B</button>
-        </div>
-        <div className="note-selection">
-          <button className='note-btn' onClick={() => selectKey('E3')}>E</button>
-          <button className='note-btn' onClick={() => selectKey('F3')}>F</button>
-          <button className='note-btn' onClick={() => selectKey('G3')}>G</button>
-          <button className='note-btn' onClick={() => selectKey('A3')}>A</button>
-          <button className='note-btn' onClick={() => selectKey('B3')}>B</button>
-        </div>
+      <div class="note-selection">
+        {renderNotes}
       </div>
       <div className="column-selection">
         <button className='note-btn' onClick={() => selectSymbol('SymbolMeasureBar')}> Measure Bar</button>
@@ -92,75 +88,144 @@ const SymbolSelector = ({ selectKey, selectNoteType, selectDotted, selectStacatt
 };
 
 const MusicSheetCreator = () => {
-  const [sheet, setSheet] = useState([DEFAULT_SYMBOL]);
-  const [index, setIndex] = useState(INDEX_ZERO);
+  const [trebleStaff, setTrebleStaff] = useState([DEFAULT_TREBLE]);
+  const [bassStaff, setBassStaff] = useState([DEFAULT_BASS]);
+  const [trebleIndex, setTrebleIndex] = useState(INDEX_ZERO);
+  const [bassIndex, setBassIndex] = useState(-1);
 
   const addNote = () => {
-    setIndex(sheet.length);
-    const updatedSheet = sheet.concat(DEFAULT_SYMBOL);
-    setSheet(updatedSheet);
+    setTrebleIndex(trebleStaff.length);
+    const updatedTrebleStaff = trebleStaff.concat(DEFAULT_TREBLE);
+    setTrebleStaff(updatedTrebleStaff);
+
+    setBassIndex(-1);
+    const updatedBassStaff = bassStaff.concat(DEFAULT_BASS);
+    setBassStaff(updatedBassStaff);
   };
 
   const removeNote = () => {
-    if (sheet.length > AT_LEAST_ONE) {
-      const updatedSheet = sheet.filter((item, i) => i !== index);
-      setSheet(updatedSheet);
-      setIndex(INDEX_ZERO);
+    if (trebleIndex !== -1 && trebleStaff.length > AT_LEAST_ONE) {
+      const updatedTrebleStaff = trebleStaff.filter((item, i) => i !== trebleIndex);
+      setTrebleStaff(updatedTrebleStaff);
+      setTrebleIndex(INDEX_ZERO);
+      const updatedBassStaff = bassStaff.filter((item, i) => i !== trebleIndex);
+      setBassStaff(updatedBassStaff);
+      setBassIndex(-1);
+    }
+    if (bassIndex !== -1 && bassStaff.length > AT_LEAST_ONE) {
+      const updatedTrebleStaff = trebleStaff.filter((item, i) => i !== bassIndex);
+      setTrebleStaff(updatedTrebleStaff);
+      setTrebleIndex(INDEX_ZERO);
+      const updatedBassStaff = bassStaff.filter((item, i) => i !== bassIndex);
+      setBassStaff(updatedBassStaff);
+      setBassIndex(-1);
     }
   };
 
   const selectKey = key => {
-    const updatedSheet = sheet.map((item, i) => {
-      const type = item.type ? item.type : 'quarter';
-      if (i === index) {
-        return {
-          ...item,
-          key,
-          type
-        };
-      }
-      else {
-        return item;
-      }
-    });
-    setSheet(updatedSheet);
+    if (trebleIndex !== -1) {
+      const updatedTrebleStaff = trebleStaff.map((item, i) => {
+        const type = item.type ? item.type : 'quarter';
+        if (i === trebleIndex) {
+          return {
+            ...item,
+            key,
+            type
+          };
+        }
+        else {
+          return item;
+        }
+      });
+      setTrebleStaff(updatedTrebleStaff);
+    }
+    if (bassIndex !== -1) {
+      const updatedBassStaff = bassStaff.map((item, i) => {
+        const type = item.type ? item.type : 'quarter';
+        if (i === bassIndex) {
+          return {
+            ...item,
+            key,
+            type
+          };
+        }
+        else {
+          return item;
+        }
+      });
+      setBassStaff(updatedBassStaff);
+    }
   };
 
   const selectNoteType = noteType => {
-    const updatedSheet = sheet.map((item, i) => {
-      if (i === index) {
-        const key = item.type ? item.key : 'C4';
-        return {
-          ...item,
-          type: noteType,
-          key
-        };
-      }
-      else {
-        return item;
-      }
-    });
-    setSheet(updatedSheet);
+    if (trebleIndex !== -1) {
+      const updatedTrebleStaff = trebleStaff.map((item, i) => {
+        if (i === trebleIndex) {
+          const key = item.type ? item.key : 'C4';
+          return {
+            ...item,
+            type: noteType,
+            key
+          };
+        }
+        else {
+          return item;
+        }
+      });
+      setTrebleStaff(updatedTrebleStaff);
+    }
+    if (bassIndex !== -1) {
+      const updatedBassStaff = bassStaff.map((item, i) => {
+        if (i === bassIndex) {
+          const key = item.type ? item.key : 'C4';
+          return {
+            ...item,
+            type: noteType,
+            key
+          };
+        }
+        else {
+          return item;
+        }
+      });
+      setBassStaff(updatedBassStaff);
+    }
   };
 
   const selectDotted = () => {
-    const updatedSheet = sheet.map((item, i) => {
-      if (i === index) {
-        return {
-          ...item,
-          dotted: !item.dotted
-        };
-      }
-      else {
-        return item;
-      }
-    });
-    setSheet(updatedSheet);
+    if (trebleIndex !== -1) {
+      const updatedTrebleStaff = trebleStaff.map((item, i) => {
+        if (i === trebleIndex) {
+          return {
+            ...item,
+            dotted: !item.dotted
+          };
+        }
+        else {
+          return item;
+        }
+      });
+      setTrebleStaff(updatedTrebleStaff);
+    }
+    if (bassIndex !== -1) {
+      const updatedBassStaff = bassStaff.map((item, i) => {
+        if (i === bassIndex) {
+          return {
+            ...item,
+            dotted: !item.dotted
+          };
+        }
+        else {
+          return item;
+        }
+      });
+      setBassStaff(updatedBassStaff);
+    }
   };
 
   const selectStacatto = () => {
-    const updatedSheet = sheet.map((item, i) => {
-      if (i === index) {
+    const updatedTrebleStaff = trebleStaff.map((item, i) => {
+      if (i === trebleIndex) {
         return {
           ...item,
           stacatto: !item.stacatto
@@ -170,35 +235,67 @@ const MusicSheetCreator = () => {
         return item;
       }
     });
-    setSheet(updatedSheet);
+    setTrebleStaff(updatedTrebleStaff);
   };
 
   const selectSymbol = key => {
-    const updatedSheet = sheet.map((item, i) => {
-      if (i === index) {
-        return {
-          ...item,
-          key,
-          dotted: false,
-          stacatto: false,
-          type: ''
-        };
-      }
-      else {
-        return item;
-      }
-    });
-    setSheet(updatedSheet);
+    if (trebleIndex !== -1) {
+      const updatedTrebleStaff = trebleStaff.map((item, i) => {
+        if (i === trebleIndex) {
+          return {
+            ...item,
+            key,
+            dotted: false,
+            stacatto: false,
+            type: ''
+          };
+        }
+        else {
+          return item;
+        }
+      });
+      setTrebleStaff(updatedTrebleStaff);
+    }
+    if (bassIndex !== -1) {
+      const updatedBassStaff = bassStaff.map((item, i) => {
+        if (i === bassIndex) {
+          return {
+            ...item,
+            key,
+            dotted: false,
+            stacatto: false,
+            type: ''
+          };
+        }
+        else {
+          return item;
+        }
+      });
+      setTrebleStaff(updatedBassStaff);
+    }
   };
 
-  const selectIndex = newIndex => {
-    setIndex(newIndex);
+  const selectTrebleItem = newIndex => {
+    setTrebleIndex(newIndex);
+    setBassIndex(-1);
+  };
+
+  const selectBassItem = newIndex => {
+    setBassIndex(newIndex);
+    setTrebleIndex(-1);
   };
 
   return (
     <div className="main">
-      <DisplaySelection sheet={sheet} currentIndex={index} selectItem={selectIndex} />
-      <SymbolSelector selectKey={selectKey} selectNoteType={selectNoteType} selectDotted={selectDotted} selectStacatto={selectStacatto} selectSymbol={selectSymbol} />
+      <DisplaySelection
+        trebleStaff={trebleStaff}
+        currentTrebleIndex={trebleIndex}
+        selectTrebleItem={selectTrebleItem}
+        bassStaff={bassStaff}
+        currentBassIndex={bassIndex}
+        selectBassItem={selectBassItem}
+      />
+      <SymbolSelector selectKey={selectKey} selectNoteType={selectNoteType} selectDotted={selectDotted} selectStacatto={selectStacatto} selectSymbol={selectSymbol} isTreble={trebleIndex !== -1} />
       <button className="add-btn" onClick={addNote}>Add New Note</button>
       <button className="add-btn" onClick={removeNote}>Remove Selected Note</button>
     </div>
