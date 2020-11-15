3910707a9c3887ab5e9b358ac09c60adcf828dda
Chris Varughese <cjamiev@icloud.com>
2020-11-14 21:53:01 -0600

    Added note modifiers.

---
+++ src/svg/DrawNoteModifier.js
@@ -0,0 +1,171 @@
+import React, { Fragment } from 'react';
+
+export const renderFlat = () => {
+  return (
+    <Fragment>
+      <ellipse
+        fill="#000000"
+        stroke-width="0.276354"
+        cx="-27.756186"
+        cy="252.28653"
+        rx="14.529974"
+        ry="8.6136208"
+        transform="matrix(0.79927148,-0.60097013,0.66436785,0.74740575,0,0)" />
+      <ellipse
+        fill="#ffffff"
+        stroke-width="0.218277"
+        cx="-3.6929584"
+        cy="251.65794"
+        rx="11.426763"
+        ry="6.8329792"
+        transform="matrix(0.82981208,-0.55804293,0.59005008,0.80736665,0,0)" />
+      <rect
+        fill="#000000"
+        stroke-width="0.264583"
+        width="2.2678571"
+        height="46.113098"
+        x="142.49707"
+        y="169.14435" />
+      <rect
+        fill="#ffffff"
+        stroke-width="0.264583"
+        width="19.654762"
+        height="30.616072"
+        x="122.84225"
+        y="189.93303" />
+    </Fragment>
+  );
+};
+
+export const renderSharp = () => {
+  return (
+    <Fragment>
+      <rect
+        fill="#000000"
+        stroke-width="0.218844"
+        width="36.608231"
+        height="2.3688653"
+        x="172.20193"
+        y="-77.374886"
+        transform="matrix(0.01088649,0.99994074,-0.99919459,0.04012683,0,0)" />
+      <rect
+        fill="#000000"
+        stroke-width="0.218844"
+        width="36.608231"
+        height="2.3688653"
+        x="174.29449"
+        y="-63.582623"
+        transform="matrix(0.01088649,0.99994074,-0.99919459,0.04012683,0,0)" />
+      <rect
+        fill="#000000"
+        stroke-width="0.293296"
+        width="33.986309"
+        height="4.5830946"
+        x="42.932903"
+        y="192.35248"
+        transform="matrix(0.97807107,-0.20827142,0.0605518,0.99816506,0,0)" />
+      <rect
+        fill="#000000"
+        stroke-width="0.296456"
+        width="34.719913"
+        height="4.583478"
+        x="41.590122"
+        y="208.72002"
+        transform="matrix(0.9789978,-0.2038708,0.06191227,0.9980816,0,0)" />
+    </Fragment>
+  );
+};
+
+export const renderNatural = () => {
+  return (
+    <Fragment>
+      <rect
+        fill="#000000"
+        stroke-width="0.20206"
+        width="18.216297"
+        height="4.0583639"
+        x="85.308434"
+        y="198.85078"
+        transform="matrix(0.97309911,-0.23038689,0.06657962,0.99778112,0,0)"
+      />
+      <rect
+        fill="#000000"
+        stroke-width="0.217714"
+        width="21.142174"
+        height="4.0595307"
+        x="78.922646"
+        y="214.57109"
+        transform="matrix(0.97795381,-0.20882131,0.08174101,0.9966536,0,0)"
+      />
+      <rect
+        fill="#000000"
+        stroke-width="0.213528"
+        width="34.871147"
+        height="2.3675177"
+        x="175.33614"
+        y="-115.97405"
+        transform="matrix(0.00213456,0.99999772,-0.99997444,0.00715038,0,0)"
+      />
+      <rect
+        fill="#000000"
+        stroke-width="0.218844"
+        width="36.608231"
+        height="2.3688653"
+        x="167.43236"
+        y="-94.94767"
+        transform="matrix(0.01086867,0.99994093,-0.99919531,0.04010902,0,0)" />
+    </Fragment>
+  );
+};
+
+export const renderFermata = () => {
+  return (
+    <Fragment>
+      <ellipse
+        fill="#000000"
+        stroke-width="0.264583"
+        cx="28.159225"
+        cy="237.74701"
+        rx="15.308036"
+        ry="12.473214" />
+      <ellipse
+        fill="#ffffff"
+        stroke-width="0.233957"
+        cx="28.159229"
+        cy="240.20387"
+        rx="14.363095"
+        ry="10.394345" />
+      <rect
+        fill="#ffffff"
+        stroke-width="0.264583"
+        width="53.483627"
+        height="14.363094"
+        x="6.2366071"
+        y="237.936" />
+      <ellipse
+        fill="#000000"
+        stroke-width="0.190675"
+        cx="28.367113"
+        cy="235.12012"
+        rx="1.9843749"
+        ry="2.1733632" />
+    </Fragment>
+  );
+};
+
+export const renderAccent = () => {
+  return (
+    <Fragment>
+      <path
+        fill="#000000"
+        stroke-width="0.264583"
+        d="m 45.26265,148.73364 -9.165436,13.91182 -7.465274,-14.89341 -7.465274,-14.89341 16.63071,0.98159 16.63071,0.98158 z"
+        transform="matrix(0.90310663,0.45525284,-0.45299356,0.89862478,70.78203,-2.3234585)" />
+      <path
+        fill="#ffffff"
+        stroke-width="0.264583"
+        d="m 45.26265,148.73364 -9.165436,13.91182 -7.465274,-14.89341 -7.465274,-14.89341 16.63071,0.98159 16.63071,0.98158 z"
+        transform="matrix(0.66100879,0.34779043,-0.33155854,0.68650443,59.241066,31.82)" />
+    </Fragment>
+  );
+};
