55109ce834858ee800ca3ed2322e273c151c9b07
Chris Varughese <cjamiev@icloud.com>
2021-02-08 22:41:44 -0600

    Making the keyboard again plus adding in web responsiveness

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { useState } from 'react';
+import Keyboard from  './components/keyboard';
 
 const App = () => {
+  const [pianoKey, setPianoKey] = useState('C4');
+
+  const selectPianoKey = (selected) => {
+    setPianoKey(selected);
+    setMusicalSymbol('');
+  };
+
   return (
-    <div>Reset</div>
+    <div>
+      <div>Selection:{pianoKey}</div>
+      <Keyboard selectPianoKey={setPianoKey} />
+    </div>
   );
 };
 
