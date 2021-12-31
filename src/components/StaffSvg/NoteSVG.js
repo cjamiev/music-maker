f66415d8bbc547275274f647ff6860e7b0770fa7
Chris Varughese <cjamiev@icloud.com>
2021-12-30 22:54:19 -0600

    Conditions for Staff Ledger lines

--- src/components/StaffSvg/NoteSVG.js
+++ src/components/StaffSvg/NoteSVG.js
@@ -132,24 +132,40 @@ export const RolledSVG = ({ transform }) => {
   );
 };
 
-export const StaffSVG = ({ transform }) => {
+const StaffTopLedger = ({ conditions = {} }) => {
+  const { showTopLedgerFive, showTopLedgerFour, showTopLedgerThree, showTopLedgerTwo, showTopLedgerOne } = conditions;
+  return (<>
+    { showTopLedgerFive && <rect data-testid="staff-line-top-ledger-five" className="svg__36" aria-label="staff line top ledger five" width="7.4552097" height="0.17916849" x="74.985931" y="76.970314" /> }
+    { showTopLedgerFour && <rect data-testid="staff-line-top-ledger-four" className="svg__36" aria-label="staff line top ledger four" width="7.4552097" height="0.17916849" x="74.985931" y="81.997368" /> }
+    { showTopLedgerThree && <rect data-testid="staff-line-top-ledger-three" className="svg__36" aria-label="staff line top ledger three" width="7.4552097" height="0.17916857" x="74.985931" y="87.024467" /> }
+    { showTopLedgerTwo && <rect data-testid="staff-line-top-ledger-two" className="svg__36" aria-label="staff line top ledger two" width="7.4552097" height="0.17916857" x="74.985931" y="92.051582" /> }
+    { showTopLedgerOne && <rect data-testid="staff-line-top-ledger-one" className="svg__36" aria-label="staff line top ledger one" width="7.4552097" height="0.17916857" x="74.985931" y="97.078636" /> }
+  </>);
+};
+
+const StaffBottomLedger = ({ conditions = {} }) => {
+  const { showBottomLedgerFive, showBottomLedgerFour, showBottomLedgerThree, showBottomLedgerTwo, showBottomLedgerOne } = conditions;
+  return (
+    <>
+      { showBottomLedgerOne && <rect data-testid="staff-line-bottom-ledger-one" className="svg__37" aria-label="staff line bottom ledger one" width="7.4552088" height="0.17916749" x="74.985931" y="127.24142" /> }
+      { showBottomLedgerTwo && <rect data-testid="staff-line-bottom-ledger-two" className="svg__36" aria-label="staff line bottom ledger two" width="7.4552097" height="0.17916857" x="74.985931" y="132.26854" /> }
+      { showBottomLedgerThree && <rect data-testid="staff-line-bottom-ledger-three" className="svg__36" aria-label="staff line bottom ledger three" width="7.4552097" height="0.17916857" x="74.985931" y="137.29558" /> }
+      { showBottomLedgerFour && <rect data-testid="staff-line-bottom-ledger-four" className="svg__36" aria-label="staff line bottom ledger four" width="7.4552097" height="0.17916857" x="74.985931" y="142.32262" /> }
+      { showBottomLedgerFive && <rect data-testid="staff-line-bottom-ledger-five" className="svg__36" aria-label="staff line bottom ledger five" width="7.4552097" height="0.17916857" x="74.985931" y="147.34975" /> }
+    </>
+  );
+};
+
+export const StaffSVG = ({ transform, conditions = {} }) => {
   return (
     <g data-testid="subcomponent-staff" transform={transform} aria-label="staff" >
+      <StaffTopLedger conditions={conditions} />
       <rect data-testid="staff-line-five" className="svg__0" aria-label="staff line five" width="27.192894" height="0.073101997" x="65.119415" y="102.15884" />
       <rect data-testid="staff-line-four" className="svg__0" aria-label="staff line four" width="27.192894" height="0.07310199" x="65.119415" y="107.18609" />
       <rect data-testid="staff-line-three" className="svg__0" aria-label="staff line three" width="27.192894" height="0.07310199" x="65.119415" y="112.21325" />
       <rect data-testid="staff-line-two" className="svg__0" aria-label="staff line two" width="27.192894" height="0.07310199" x="65.119415" y="117.24029" />
       <rect data-testid="staff-line-one" className="svg__0" aria-label="staff line one" width="27.192894" height="0.073101684" x="65.117096" y="122.26739" />
-      <rect data-testid="staff-line-top-ledger-three" className="svg__36" aria-label="staff line top ledger three" width="7.4552097" height="0.17916857" x="74.985931" y="87.024467" />
-      <rect data-testid="staff-line-top-ledger-two" className="svg__36" aria-label="staff line top ledger two" width="7.4552097" height="0.17916857" x="74.985931" y="92.051582" />
-      <rect data-testid="staff-line-top-ledger-one" className="svg__36" aria-label="staff line top ledger one" width="7.4552097" height="0.17916857" x="74.985931" y="97.078636" />
-      <rect data-testid="staff-line-top-ledger-four" className="svg__36" aria-label="staff line top ledger four" width="7.4552097" height="0.17916849" x="74.985931" y="81.997368" />
-      <rect data-testid="staff-line-top-ledger-five" className="svg__36" aria-label="staff line top ledger five" width="7.4552097" height="0.17916849" x="74.985931" y="76.970314" />
-      <rect data-testid="staff-line-bottom-ledger-one" className="svg__37" aria-label="staff line bottom ledger one" width="7.4552088" height="0.17916749" x="74.985931" y="127.24142" />
-      <rect data-testid="staff-line-bottom-ledger-two" className="svg__36" aria-label="staff line bottom ledger two" width="7.4552097" height="0.17916857" x="74.985931" y="132.26854" />
-      <rect data-testid="staff-line-bottom-ledger-five" className="svg__36" aria-label="staff line bottom ledger five" width="7.4552097" height="0.17916857" x="74.985931" y="147.34975" />
-      <rect data-testid="staff-line-bottom-ledger-three" className="svg__36" aria-label="staff line bottom ledger three" width="7.4552097" height="0.17916857" x="74.985931" y="137.29558" />
-      <rect data-testid="staff-line-bottom-ledger-four" className="svg__36" aria-label="staff line bottom ledger four" width="7.4552097" height="0.17916857" x="74.985931" y="142.32262" />
+      <StaffBottomLedger conditions={conditions} />
     </g>
   );
 };
---
+++ src/constants/stafflines.js
@@ -0,0 +1,114 @@
+
+export const mapStaffLines = {
+  'A3': {
+    showBottomLedgerFive: true,
+    showBottomLedgerFour: true,
+    showBottomLedgerThree: true,
+    showBottomLedgerTwo: true,
+    showBottomLedgerOne: true
+  },
+  'B3': {
+    showBottomLedgerFive: true,
+    showBottomLedgerFour: true,
+    showBottomLedgerThree: true,
+    showBottomLedgerTwo: true,
+    showBottomLedgerOne: true
+  },
+  'C3': {
+    showBottomLedgerFour: true,
+    showBottomLedgerThree: true,
+    showBottomLedgerTwo: true,
+    showBottomLedgerOne: true
+  },
+  'D3': {
+    showBottomLedgerFour: true,
+    showBottomLedgerThree: true,
+    showBottomLedgerTwo: true,
+    showBottomLedgerOne: true
+  },
+  'E3': {
+    showBottomLedgerThree: true,
+    showBottomLedgerTwo: true,
+    showBottomLedgerOne: true
+  },
+  'F3': {
+    showBottomLedgerThree: true,
+    showBottomLedgerTwo: true,
+    showBottomLedgerOne: true
+  },
+  'G3': {
+    showBottomLedgerTwo: true,
+    showBottomLedgerOne: true
+  },
+  'A4': {
+    showBottomLedgerTwo: true,
+    showBottomLedgerOne: true
+  },
+  'B4': {
+    showBottomLedgerOne: true
+  },
+  'C4': {
+    showBottomLedgerOne: true
+  },
+  'D4': {},
+  'E4': {},
+  'F4': {},
+  'G4': {},
+  'A5': {},
+  'B5': {},
+  'C5': {},
+  'D5': {},
+  'E5': {},
+  'F5': {},
+  'G5': {},
+  'A6': {
+    showTopLedgerOne: true
+  },
+  'B6': {
+    showTopLedgerOne: true
+  },
+  'C6': {
+    showTopLedgerTwo: true,
+    showTopLedgerOne: true
+  },
+  'D6': {
+    showTopLedgerTwo: true,
+    showTopLedgerOne: true
+  },
+  'E6': {
+    showTopLedgerThree: true,
+    showTopLedgerTwo: true,
+    showTopLedgerOne: true
+  },
+  'F6': {
+    showTopLedgerThree: true,
+    showTopLedgerTwo: true,
+    showTopLedgerOne: true
+  },
+  'G6': {
+    showTopLedgerFour: true,
+    showTopLedgerThree: true,
+    showTopLedgerTwo: true,
+    showTopLedgerOne: true
+  },
+  'A7': {
+    showTopLedgerFour: true,
+    showTopLedgerThree: true,
+    showTopLedgerTwo: true,
+    showTopLedgerOne: true
+  },
+  'B7': {
+    showTopLedgerFive: true,
+    showTopLedgerFour: true,
+    showTopLedgerThree: true,
+    showTopLedgerTwo: true,
+    showTopLedgerOne: true
+  },
+  'C7': {
+    showTopLedgerFive: true,
+    showTopLedgerFour: true,
+    showTopLedgerThree: true,
+    showTopLedgerTwo: true,
+    showTopLedgerOne: true
+  }
+};
