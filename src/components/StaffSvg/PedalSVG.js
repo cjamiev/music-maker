b9bf188b4dd6ca3e2cc650c00c16af2a47d34d93
Chris Varughese <cjamiev@icloud.com>
2022-01-13 14:28:30 -0600

    Fixing Pedal start and end locations

--- src/components/StaffSvg/PedalSVG.js
+++ src/components/StaffSvg/PedalSVG.js
@@ -16,18 +16,22 @@ export const PedalContinueSVG = ({ transform }) => {
 
 export const PedalStartSVG = ({ transform }) => {
   return (
-    <g data-testid="subcomponent-pedal-start" transform={transform} aria-label="pedal start" transform="matrix(0.98982022,0,0,1,-100.97415,79.212368)" >
-      <rect data-testid="rect1607" className="svg__10" width="28.066965" height="0.62309307" x="167.50221" y="114.50225" />
-      <rect data-testid="rect1610" className="svg__10" width="4.2280054" height="0.4762491" x="-114.58158" y="167.50221" transform="rotate(-90)" />
+    <g data-testid="subcomponent-pedal-start" transform={transform} aria-label="pedal start" >
+      <g transform="matrix(0.98982022,0,0,1,-91.97415,79.212368)" >
+        <rect data-testid="rect1607" className="svg__10" width="19.066965" height="0.62309307" x="167.50221" y="114.50225" />
+        <rect data-testid="rect1610" className="svg__10" width="4.2280054" height="0.4762491" x="-114.58158" y="167.50221" transform="rotate(-90)" />
+      </g>
     </g>
   );
 };
 
 export const PedalEndSVG = ({ transform }) => {
   return (
-    <g data-testid="subcomponent-pedal-end" transform={transform} aria-label="pedal end" transform="matrix(-0.98982021,0,0,1,258.40124,79.212328)" >
-      <rect data-testid="rect1607-3" className="svg__10" width="28.066965" height="0.62309307" x="167.50221" y="114.50225" />
-      <rect data-testid="rect1610-6" className="svg__10" width="4.2280054" height="0.4762491" x="-114.58158" y="167.50221" transform="rotate(-90)" />
+    <g data-testid="subcomponent-pedal-end" transform={transform} aria-label="pedal end" >
+      <g transform="matrix(-0.98982021,0,0,1,249.40124,79.212328)" >
+        <rect data-testid="rect1607-3" className="svg__10" width="19.066965" height="0.62309307" x="167.50221" y="114.50225" />
+        <rect data-testid="rect1610-6" className="svg__10" width="4.2280054" height="0.4762491" x="-114.58158" y="167.50221" transform="rotate(-90)" />
+      </g>
     </g>
   );
 };
