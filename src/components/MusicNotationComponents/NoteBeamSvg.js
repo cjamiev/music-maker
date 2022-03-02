97569bf669f9f7b1c81c9719c1620bc44c51e800
Chris Varughese <cjamiev@icloud.com>
2022-03-02 09:33:12 -0600

    continued beam testing added stems

--- src/components/MusicNotationComponents/NoteBeamSvg.js
+++ src/components/MusicNotationComponents/NoteBeamSvg.js
@@ -91,17 +91,45 @@ const BeamPath = ({ flippedNotes, widthGap, baseY, angleHeightModifier }) => {
   return (<path className="svg-primary-color svg__20 svg--marked-color" d={pathDefinition} />);
 };
 
+const BeamStemConnector = ({ beamNotes, baseY, angleHeightModifier }) => {
+  const firstNotePosition = NOTE_STEM_BASE_Y + mapNotePosition[beamNotes[ZERO].pianoKey];
+  const direction = angleHeightModifier > ZERO ? ONE : -ONE;
+  return beamNotes.map((noteData,index) => {
+    const heightModifier = angleHeightModifier < ZERO ? index : (beamNotes.length - index - ONE);
+    const shiftX = NOTE_STEM_BASE_X + index * STAFF_LINE_WIDTH/TWO;
+    const height = heightModifier * DISTANCE_BETWEEN_STAFF_LINES + NOTE_STEM_BASE_Y + mapNotePosition[noteData.pianoKey] - firstNotePosition;
+    const shiftY = baseY + direction * index * DISTANCE_BETWEEN_STAFF_LINES;
+
+    return <rect
+      key={shiftX}
+      data-testid="beam-note-stem"
+      className="svg__20 svg--marked-color"
+      aria-label="beam note stem"
+      width="0.45734397"
+      height={height}
+      x={shiftX}
+      y={shiftY}
+    />;
+  });
+};
+
 const NoteBeamSvg = ({ transform, content = { } }) => {
-  const widthGap = content.beamNotes.length - ONE;
-  const flippedNotes = getAreNotesFlipped(content.beamNotes);
-  const baseY = getBaseY(content.beamNotes);
-  const angleHeightModifier = getAngleHeightModifier(content.beamNotes);
+  const { beamNotes } = content;
+  const widthGap = beamNotes.length - ONE;
+  const flippedNotes = getAreNotesFlipped(beamNotes);
+  const baseY = getBaseY(beamNotes);
+  const angleHeightModifier = getAngleHeightModifier(beamNotes);
 
   return (
     <g transform={transform}>
       <BeamPath
         flippedNotes={flippedNotes}
         widthGap={widthGap}
+        baseY={baseY}
+        angleHeightModifier={angleHeightModifier}
+      />
+      <BeamStemConnector
+        beamNotes={beamNotes}
         baseY={baseY}
         angleHeightModifier={angleHeightModifier}
       />
