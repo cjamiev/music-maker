58d6e58f4f99c16a0d7a06de97812eb072f799d4
Chris Varughese <cjamiev@icloud.com>
2022-03-09 21:42:05 -0600

    refactoring beam

--- src/components/MusicNotationComponents/NoteBeam/BeamPath.js
+++ src/components/MusicNotationComponents/NoteBeam/BeamPath.js
@@ -1,49 +1,35 @@
-/* eslint-disable no-magic-numbers */
 import React from 'react';
-import { pianoKeyList, pianoKeyListWithoutAccidentals } from 'constants/pianokeys';
 import {
   STAFF_LINE_WIDTH,
-  DISTANCE_BETWEEN_STAFF_LINES,
   NOTE_STEM_BASE_X,
-  NOTE_STEM_BASE_Y,
-  NOTE_STEM_FLIPPED_BASE_X,
-  NOTE_STEM_FLIPPED_BASE_Y,
   NOTE_STEM_WIDTH,
-  NOTE_FLAG_WIDTH,
-  NOTE_STEM_HEIGHT,
-  NOTE_BEAM_HEIGHT
+  NOTE_BEAM_HEIGHT,
+  NOTE_FLAG_WIDTH
 } from 'constants/svgattributes';
-import {
-  mapNotePosition
-} from 'constants/stafflines';
 
 const ZERO = 0;
 const ONE = 1;
 const TWO = 2;
-const THREE = 3;
-const FOUR = 4;
-const STAFF_MIDPOINT = 26;
 
-const BeamPath = ({ flippedNotes, widthGap, baseY, angleHeightModifier }) => {
-  const shiftXForFirstFlipped = flippedNotes[ZERO] ? NOTE_FLAG_WIDTH - TWO * NOTE_STEM_WIDTH: ZERO;
-  const shiftXForLastFlipped = flippedNotes[widthGap] ? NOTE_FLAG_WIDTH - TWO * NOTE_STEM_WIDTH : ZERO;
-  const shiftYForFlipped = flippedNotes[ZERO] ? 50 : ZERO;
+const BeamPath = ({ isBeamOnTop, beamCoordinates }) => {
+  const shiftX = isBeamOnTop ? ZERO : NOTE_FLAG_WIDTH - TWO * NOTE_STEM_WIDTH;
+  const widthGap = beamCoordinates.length - ONE;
 
   const originCoord = {
-    x: NOTE_STEM_BASE_X - shiftXForFirstFlipped,
-    y: baseY - NOTE_BEAM_HEIGHT + shiftYForFlipped
+    x: NOTE_STEM_BASE_X - shiftX,
+    y: beamCoordinates[ZERO]
   };
   const secondCoord = {
-    x: NOTE_STEM_BASE_X - shiftXForFirstFlipped,
-    y: baseY + shiftYForFlipped
+    x: NOTE_STEM_BASE_X - shiftX,
+    y: beamCoordinates[ZERO] - NOTE_BEAM_HEIGHT
   };
   const thirdCoord = {
-    x: NOTE_STEM_BASE_X + NOTE_STEM_WIDTH + STAFF_LINE_WIDTH * widthGap/TWO - shiftXForLastFlipped,
-    y: baseY + angleHeightModifier + shiftYForFlipped
+    x: NOTE_STEM_BASE_X + NOTE_STEM_WIDTH + STAFF_LINE_WIDTH * widthGap/TWO - shiftX,
+    y: beamCoordinates[beamCoordinates.length - ONE] - NOTE_BEAM_HEIGHT
   };
   const finalCoord = {
-    x: NOTE_STEM_BASE_X + STAFF_LINE_WIDTH * widthGap/TWO + NOTE_STEM_WIDTH - shiftXForLastFlipped,
-    y: baseY + angleHeightModifier - NOTE_BEAM_HEIGHT + shiftYForFlipped
+    x: NOTE_STEM_BASE_X + STAFF_LINE_WIDTH * widthGap/TWO + NOTE_STEM_WIDTH - shiftX,
+    y: beamCoordinates[beamCoordinates.length - ONE]
   };
 
   const pathDefinition = `
