3eb5de7549aad2fd5de460cf061f781d2e0e43f0
Chris Varughese <cjamiev@icloud.com>
2022-03-10 09:06:59 -0600

    continued refactor of note beam

--- src/components/MusicNotationComponents/NoteBeam/helper.js
+++ src/components/MusicNotationComponents/NoteBeam/helper.js
@@ -12,7 +12,7 @@ const ONE = 1;
 const TWO = 2;
 const STAFF_MIDPOINT = 26;
 
-const getBeamPositions = (beamNotes) => beamNotes.map(noteData => pianoKeyListWithoutAccidentals.findIndex(key => key === noteData.pianoKey));
+const getBeamNotePositions = (beamNotes) => beamNotes.map(noteData => pianoKeyListWithoutAccidentals.findIndex(key => key === noteData.pianoKey));
 
 const getIsBeamOnTop = (beamNotePositions) => {
   return beamNotePositions
@@ -32,24 +32,31 @@ const getBeamAngle = (beamNotePositions) => {
   return magnitude > ZERO ? ONE : -ONE;
 };
 
-const getBeamHeights = (beamNoteHeights, beamAngle) => {
-  if(beamAngle === ZERO) {
-    return Array.from({ length: beamNoteHeights.length }).map((_) => beamNoteHeights[ZERO]);
-  } else if(beamAngle < ZERO) {
-    return Array.from({ length: beamNoteHeights.length }).map((_, i) => beamNoteHeights[ZERO] + i * DISTANCE_BETWEEN_STAFF_LINES);
-  } else {
-    return Array.from({ length: beamNoteHeights.length }).map((_, i) => beamNoteHeights[ZERO] - i * DISTANCE_BETWEEN_STAFF_LINES);
-  }
-};
-
-const getBeamCoordinates = (isBeamOnTop, beamAngle, beamNotes) => {
-  const beamNoteHeights = beamNotes.map(noteData => {
+const getBeamNoteHeights = (beamNotes, isBeamOnTop) => {
+  return beamNotes.map(noteData => {
     return isBeamOnTop
       ? NOTE_STEM_BASE_Y + mapNotePosition[noteData.pianoKey]
       : NOTE_STEM_FLIPPED_BASE_Y + mapNotePosition[noteData.pianoKey] + NOTE_STEM_HEIGHT;
   });
-  const baseY = beamNoteHeights[ZERO];
-  const beamHeights = getBeamHeights(beamNoteHeights, beamAngle);
+};
+
+const getBeamHeights = (beamNoteHeights, beamAngle) => {
+  if(beamAngle === ZERO) {
+    return Array
+      .from({ length: beamNoteHeights.length })
+      .map((_) => beamNoteHeights[ZERO]);
+  } else if(beamAngle < ZERO) {
+    return Array
+      .from({ length: beamNoteHeights.length })
+      .map((_, i) => beamNoteHeights[ZERO] + i * DISTANCE_BETWEEN_STAFF_LINES);
+  } else {
+    return Array
+      .from({ length: beamNoteHeights.length })
+      .map((_, i) => beamNoteHeights[ZERO] - i * DISTANCE_BETWEEN_STAFF_LINES);
+  }
+};
+
+const getBeamCoordinates = (beamHeights, beamNoteHeights, isBeamOnTop) => {
   const beamDiffs = isBeamOnTop
     ? beamHeights.map((h,i) => beamNoteHeights[i] - h)
     : beamHeights.map((h,i) => h - beamNoteHeights[i]);
@@ -58,15 +65,18 @@ const getBeamCoordinates = (isBeamOnTop, beamAngle, beamNotes) => {
     ? beamHeights.map((h) => h + shiftY)
     : beamHeights.map((h) => h - shiftY);
 
-  return { beamCoordinates, beamNoteHeights, isBeamOnTop };
+  return beamCoordinates;
 };
 
 const getBeamData = (beamNotes) => {
-  const beamNotePositions = getBeamPositions(beamNotes);
+  const beamNotePositions = getBeamNotePositions(beamNotes);
   const isBeamOnTop = getIsBeamOnTop(beamNotePositions);
   const beamAngle = getBeamAngle(beamNotePositions);
+  const beamNoteHeights = getBeamNoteHeights(beamNotes, isBeamOnTop);
+  const beamHeights = getBeamHeights(beamNoteHeights, beamAngle);
+  const beamCoordinates = getBeamCoordinates(beamHeights, beamNoteHeights, isBeamOnTop);
 
-  return getBeamCoordinates(isBeamOnTop, beamAngle, beamNotes);
+  return { beamCoordinates, beamNoteHeights, isBeamOnTop };
 };
 
 export default getBeamData;
