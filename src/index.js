57928147e419cd5e6ab999c7009611936ac008e6
Chris Varughese <cjamiev@gmail.com>
2023-04-02 15:45:00 -0500

    feat: upgrading to react 18 style

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from 'core';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const reactRoot = ReactDOM.createRoot(rootElement);
+
+reactRoot.render(
   <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
