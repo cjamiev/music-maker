950c8e494b5366dbbb3dbf49a8d99d8001dc53e8
Chris Varughese <cjamiev@gmail.com>
2023-04-09 12:09:05 -0500

    chore: using icons for column section

--- src/components/pages/CreateMusic/ColumnPositionController.js
+++ src/components/pages/CreateMusic/ColumnPositionController.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Button } from 'components/atoms/Button';
+import { Button, IconButton } from 'components/atoms/Button';
+import { ICON_TYPES, ICON_SIZES } from 'constants/icon';
 
 const ZERO = 0;
 const ONE = 1;
@@ -10,12 +11,12 @@ export const ColumnPositionController = ({ editorPosition, data, onChange }) => 
   const lastColumnIndex = isBassSelection ? currentLine.bass.length - ONE : currentLine.treble.length - ONE;
 
   return (
-    <div className='createmusic__btn-group'>
-      <Button
-        label='Add'
-        className='createmusic__btn'
-        classColor='primary'
-        onClick={() => {
+    <div className='createmusic__col-modifier'>
+      <IconButton
+        className="createmusic__col-modifier--add"
+        type={ICON_TYPES.PLUS}
+        size={ICON_SIZES.EXTRA_SMALL}
+        handleClick={() => {
           const nextColumnIndex = lastColumnIndex + ONE;
           const noteId = `${pageIndex},${lineIndex},${nextColumnIndex}`;
           const newNote = {
@@ -56,84 +57,91 @@ export const ColumnPositionController = ({ editorPosition, data, onChange }) => 
           onChange(updatedEditorPosition, updatedData);
         }}
       />
-      <Button
-        label='Prev'
-        className='createmusic__btn'
-        classColor='primary'
-        disabled={columnIndex === ZERO}
-        onClick={() => {
-          onChange({
-            ...editorPosition,
-            columnIndex: columnIndex - ONE
-          });
-        }}
-      />
-      <Button
-        label='Next'
-        className='createmusic__btn'
-        classColor='primary'
-        disabled={columnIndex === lastColumnIndex}
-        onClick={() => {
-          onChange({
-            ...editorPosition,
-            columnIndex: columnIndex + ONE
-          });
-        }}
-      />
-      <Button
-        label='Delete'
-        className='createmusic__btn'
-        classColor='primary'
-        disabled={lastColumnIndex === ZERO}
-        onClick={() => {
-          const currentSection = isBassSelection ? currentLine.bass : currentLine.treble;
-          const updatedSection = currentSection
-            .filter((item) => !(item.columnIndex === columnIndex))
-            .map((item,index) => {
-              const noteId = `${item.pageIndex},${item.lineIndex},${index}`;
-              return {
-                ...item,
-                id: noteId,
-                columnIndex: index
-              };
+      <div className="createmusic__col-modifier--change">
+        <IconButton
+          className="createmusic__col-modifier--left"
+          type={ICON_TYPES.ARROW}
+          size={ICON_SIZES.EXTRA_SMALL}
+          direction='LEFT'
+          isDisabled={columnIndex === ZERO}
+          handleClick={() => {
+            onChange({
+              ...editorPosition,
+              columnIndex: columnIndex - ONE
             });
+          }}
+        />
+        <IconButton
+          className="createmusic__col-modifier--right"
+          type={ICON_TYPES.ARROW}
+          size={ICON_SIZES.EXTRA_SMALL}
+          direction='RIGHT'
+          isDisabled={columnIndex === lastColumnIndex}
+          handleClick={() => {
+            onChange({
+              ...editorPosition,
+              columnIndex: columnIndex + ONE
+            });
+          }}
+        />
+      </div>
+      <div className="createmusic__col-modifier--delete">
+        <IconButton
+          type={ICON_TYPES.DELETE}
+          size={ICON_SIZES.EXTRA_SMALL}
+          isDisabled={columnIndex === ZERO}
+          handleClick={() => {
+            const currentSection = isBassSelection ? currentLine.bass : currentLine.treble;
+            const updatedSection = currentSection
+              .filter((item) => !(item.columnIndex === columnIndex))
+              .map((item,index) => {
+                const noteId = `${item.pageIndex},${item.lineIndex},${index}`;
+                return {
+                  ...item,
+                  id: noteId,
+                  columnIndex: index
+                };
+              });
 
-          const updatedLine = isBassSelection
-            ? {
-              ...currentLine,
-              bass: updatedSection
-            }
-            : {
-              ...currentLine,
-              treble: updatedSection
+            const updatedLine = isBassSelection
+              ? {
+                ...currentLine,
+                bass: updatedSection
+              }
+              : {
+                ...currentLine,
+                treble: updatedSection
+              };
+
+            const updatedData = data.map((item, index) => {
+              if(index === lineIndex) {
+                return updatedLine;
+              }
+              return item;
+            });
+            const updatedEditorPosition = {
+              ...editorPosition,
+              columnIndex: columnIndex === lastColumnIndex ? columnIndex - ONE: columnIndex
             };
 
-          const updatedData = data.map((item, index) => {
-            if(index === lineIndex) {
-              return updatedLine;
-            }
-            return item;
-          });
-          const updatedEditorPosition = {
-            ...editorPosition,
-            columnIndex: columnIndex === lastColumnIndex ? columnIndex - ONE: columnIndex
-          };
-
-          onChange(updatedEditorPosition, updatedData);
-        }}
-      />
-      <Button
-        label={isBassSelection ? 'Treble Mode' : 'Bass Mode'}
-        className='createmusic__btn'
-        classColor='primary'
-        onClick={() => {
-          onChange({
-            ...editorPosition,
-            columnIndex: ZERO,
-            isBassSelection: !isBassSelection
-          });
-        }}
-      />
+            onChange(updatedEditorPosition, updatedData);
+          }}
+        />
+      </div>
+      <div className="createmusic__col-modifier--modes">
+        <Button
+          label={isBassSelection ? 'Treble Mode' : 'Bass Mode'}
+          className='createmusic__btn'
+          classColor='primary'
+          onClick={() => {
+            onChange({
+              ...editorPosition,
+              columnIndex: ZERO,
+              isBassSelection: !isBassSelection
+            });
+          }}
+        />
+      </div>
     </div>
   );
 };
